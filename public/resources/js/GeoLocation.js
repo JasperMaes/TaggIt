700612844dba6ffe1861137e7f4ab7cde83e7f61
Jasper Maes <jasper.maes@gmail.com>
2017-04-24 22:49:17 +0200

    Added GPS error handling for map page, added filter button to screen

--- public/resources/js/GeoLocation.js
+++ public/resources/js/GeoLocation.js
@@ -23,7 +23,7 @@ var GeoLocation = (function() {
    * @returns {promise} Resolves to the output of navigator.geolocation.watchPosition
    */
   function watchLocation(successCallBack, errorCallBack) {
-    return navigator.geolocation.watchPosition(successCallBack, errorCallBack, {
+    return navigator.geolocation.watchPosition(successCallBack, errorCallBack || printError, {
       enableHighAccuracy: true,
       maximumAge: 0
     });
@@ -38,10 +38,29 @@ var GeoLocation = (function() {
     return navigator.geolocation.clearWatch(watchId);
   }
 
+  function printError(error) {
+    console.log(error)
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        console.log("User denied the request for Geolocation.")
+        break;
+      case error.POSITION_UNAVAILABLE:
+        console.log("Location information is unavailable.")
+        break;
+      case error.TIMEOUT:
+        console.log("The request to get user location timed out.")
+        break;
+      case error.UNKNOWN_ERROR:
+        console.log("An unknown error occurred.")
+        break;
+    }
+  }
+
   return {
     get: getPreciseLocation,
     watch: watchLocation,
     clearWatch: clearWatch,
-    isSupported: (!!navigator.geolocation)
+    isSupported: (!!navigator.geolocation),
+    printError: printError
   }
 })()
