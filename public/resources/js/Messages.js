1b0dc3add35c17e49501aa2746c232ebc61a4a14
Jasper Maes <jasper.maes@gmail.com>
2017-05-06 21:26:21 +0200

    Defined some error messages

---
+++ public/resources/js/Messages.js
@@ -0,0 +1,14 @@
+var Message = {
+  LocalForageInitComplete: {
+    id: "LocalForageInitComplete",
+    message: "LocalForage initialization complete."
+  },
+  TripExists: {
+    id: "TripExists",
+    message: "Trip already exists, try another ID."
+  },
+  NotImplemented: {
+    id: "NotImplemented",
+    message: "This functionality is not yet implemented."
+  }
+}
